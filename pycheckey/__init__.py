0533cc8c747f76fc56df6858da05c49bcb746055
Daniel Starner <dstarner@bloomberg.net>
2018-09-25 09:41:38 -0400

    Version bump to 0.3

--- pycheckey/__init__.py
+++ pycheckey/__init__.py
@@ -1,5 +1,5 @@
 __name__ = 'pycheckey'
-__version__ = '0.2'
+__version__ = '0.3'
 __author__ = 'Daniel Starner'
 __email__ = 'starner.daniel5@gmail.com'
 __description__ = 'Ensure nested dictionary structure.'
